b758ce93e7a84107deae71062e428b6216e799ce
gepirvu <pirvugeorgian.bi@gmail.com>
2025-01-26 22:06:53 +0100

    Fix bug in data processing pipeline

--- inference_test.py
+++ inference_test.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 	curl -X POST \ 
-	-H "Authorization: Bearer xxxxxx" \ 
+	-H "Authorization: Bearer xxxxxxx" \ 
 	-H "Content-Type: application/json" \ 
 	-d '{"inputs":[{"island": "Biscoe", "culmen_length_mm": 48.6, "culmen_depth_mm": 16.0, "flipper_length_mm": 230.0, "body_mass_g": 5800.0, "sex": "MALE"}]}' \ 
 https://adb-xxxx.azuredatabricks.net/serving-endpoints/penguings_predicitons_live/invocations
